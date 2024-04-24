6e45bbafc0ee8d4bb57125848428eede2bb59ebb
IgorTerriaga <igsantos1996@gmail.com>
2024-04-24 11:09:55 -0300

    start nest aplication

---
+++ src/categories/categories.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private prisma: PrismaService) {
+
+  }
+  create(createCategoryDto: CreateCategoryDto) {
+    return this.prisma.categories.create({
+      data: {
+        name: createCategoryDto.name
+      },
+    });
+  }
+
+  findAll() {
+    return `This action returns all categories`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} category`;
+  }
+
+  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    return `This action updates a #${id} category`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} category`;
+  }
+}
